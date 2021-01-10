12c0a2a661c990d7807ff90bcc3204fd772f0725
Monk, Andy <czech.monk90@gmail.com>
2021-01-10 02:49:47 -0800

    returns

--- admin_tools/remove_user.sh
+++ admin_tools/remove_user.sh
@@ -6,10 +6,10 @@ read confirm
 if [ $confirm == "y" ]; then
     read -p "Username: " username
 
-    printf "\033[93m[INFO]\033[0m Removing user $username."
+    printf "\033[93m[INFO]\033[0m Removing user $username.\n"
     sudo userdel --remove $username
 
-    printf "\033[93m[INFO]\033[0m Removing /home/$username directory."
+    printf "\033[93m[INFO]\033[0m Removing /home/$username directory.\n"
     sudo rm -rf /home/$username
 
     printf "\033[93m[INFO]\033[0m The user \033[91m$username\033[0m has been successfully removed.\n"
